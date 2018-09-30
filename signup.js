c4298224a348438c3b68a907038f58208c0f59cf
Yusei Ito <yuseikun@outlook.jp>
2018-09-30 21:22:05 +0900

    保護者用サインアップページ

--- signup.js
+++ signup.js
@@ -1,9 +1,9 @@
 function Signup() {
 
 
-    if (document.getElementById('password').value == document.getElementById("password_verify")) {
+    if (document.getElementById('password').value == document.getElementById("password_verify").value) {
 
-        firebase.auth().createUserWithEmailAndPassword(document.getElementById("username").value, document.getElementById("password")).catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(document.getElementById("username").value, document.getElementById("password").value).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
@@ -11,8 +11,12 @@ function Signup() {
             console.log("[Firebase Auth Error]" + errorcode + ":" + errorMessage);
             // ...
         });
+        console.log("Authentication finished");
 
 
+    } else {
+
+        alert("パスワードを確認してください。");
     }
 
 
